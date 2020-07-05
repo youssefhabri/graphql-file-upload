a140bb022d78522229ecb008aebc72b8ff975995
Youssef Habri <youssef.habri@gmail.com>
2020-07-05 16:17:23 +0100

    Cleanup `upload_spec_plug.ex`

--- lib/file_upload_web/plugs/upload_spec_plug.ex
+++ lib/file_upload_web/plugs/upload_spec_plug.ex
@@ -4,35 +4,52 @@ defmodule FileUploadWeb.Plugs.UploadSpecPlug do
   multipart request spec to upload files, to Absinthe's upload format
   """
 
-  # import Plug.Conn
-
   def init(options) do
-    options
+    Keyword.put_new(options, :path, "/graphql")
   end
 
   def call(conn, opts) do
-    path = opts[:path] || "/graphql"
+    if is_graphql_request_with_operations(conn, opts) do
+      operations = update_variables_in_operations(conn)
 
-    if conn.request_path == path and Map.has_key?(conn.params, "operations") do
-      operations = Jason.decode!(conn.params["operations"])
-
-      # If there's a files map, map it to variables
-      variables =
-        if Map.has_key?(conn.params, "map") do
-          Jason.decode!(conn.params["map"])
-          |> map_to_variables()
-        else
-          %{}
-        end
-
-      operations = %{operations | "variables" => variables}
-
-      params = Map.merge(conn.params, operations)
-      body_params = Map.merge(conn.body_params, operations)
-
-      %{conn | params: params, body_params: body_params}
+      merge_operations_into_params(conn, operations)
     else
       conn
+    end
+  end
+
+  defp is_graphql_request_with_operations(conn, opts) do
+    conn.request_path == opts[:path] and Map.has_key?(conn.params, "operations")
+  end
+
+  defp merge_operations_into_params(conn, {:ok, operations}) do
+    params = Map.merge(conn.params, operations)
+    body_params = Map.merge(conn.body_params, operations)
+
+    %{conn | params: params, body_params: body_params}
+  end
+
+  defp merge_operations_into_params(conn, {:error, _}) do
+    conn
+  end
+
+  defp update_variables_in_operations(conn) do
+    variables = map_files_to_variables(conn)
+
+    case Jason.decode(conn.params["operations"]) do
+      {:ok, operations} -> {:ok, %{operations | "variables" => variables}}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp map_files_to_variables(conn) do
+    if Map.has_key?(conn.params, "map") do
+      case Jason.decode(conn.params["map"]) do
+        {:ok, map} -> map_to_variables(map)
+        {:error, _} -> %{}
+      end
+    else
+      %{}
     end
   end
 
